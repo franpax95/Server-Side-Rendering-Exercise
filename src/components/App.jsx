249a6e715c75c852c46ad05504aa72027de8a25c
Palumbo <fcojnavarrowork@gmail.com>
2020-05-23 11:37:32 +0200

    Initial webpack configuration & production build

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <>
+            <div>Hello world!! This is for the SSR guide.</div>
+            <img src="/static/doggyfamily.jpg" alt="doggyfamily" />
+        </>
+    );
+}
